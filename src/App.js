fdf32f943b36b8db88b43beb6ab568266425c288
Marinna Vitoriano <marinna@outlook.com>
2022-10-10 18:24:32 -0300

    requisito7

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Cart from './components/Cart';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/cart" component={ Cart } />
           <Route exact path="/" component={ Home } />
+          <Route path="/product-details/:id" component={ ProductDetails } />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/FoundProducts.js
+++ src/components/FoundProducts.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { getProductById } from '../services/api';
 
 class FoundProducts extends Component {
+  handleClick = async ({ target }) => {
+    const result = await getProductById(target.id);
+    console.log(target);
+    return result;
+  };
+
   render() {
     const { productList } = this.props;
     return (
@@ -9,6 +17,18 @@ class FoundProducts extends Component {
         { productList.map((produto) => (
           <div key={ produto.id } data-testid="product">
             <span>{produto.title}</span>
+            <button
+              type="button"
+              onClick={ this.handleClick }
+            >
+              <Link
+                to={ `/product-details/${produto.id}` }
+                data-testid="product-detail-link"
+                id={ produto.id }
+              >
+                Detalhes do Produto
+              </Link>
+            </button>
           </div>)) }
       </div>
     );
